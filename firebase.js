2f6a1e1fd307c31b7a645c8418992bb905f623ac
nanapapa23 <jaeyong0001@gmail.com>
2026-05-31 23:41:04 +0900

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDkf11w1Bu5IZ_sggCT_GqMjTKnFy9fyuE",
+  authDomain: "chilgokbookcafe.firebaseapp.com",
+  projectId: "chilgokbookcafe",
+  storageBucket: "chilgokbookcafe.firebasestorage.app",
+  messagingSenderId: "150579513603",
+  appId: "1:150579513603:web:bc76fe94bbddf244084136"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
